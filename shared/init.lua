1f715ac03f67a26f1437fd885e260c1b973938ea
LifeGoal <35453587+LifeGoal@users.noreply.github.com>
2024-08-24 17:43:31 +0200

    Added Elite.Locale system

--- shared/init.lua
+++ shared/init.lua
@@ -1,4 +1,9 @@
-Elite = { cache = {} }
+Elite = {
+    Locale = {},
+    cache = {}
+}
+Elite.Locale.__index = Elite.Locale
+
 local LibConfig = {
     Settings = {
         Debug = false,
@@ -30,7 +35,7 @@ local LibConfig = {
                 "qb",
                 "qbox"
         ]]
-        Utils = "utils" -- Don't change this!
+        Utils = "utils" -- Don"t change this!
     },
     Modules = {
         Framework = {
@@ -275,3 +280,106 @@ local function setup()
 end
 
 setup()
+
+local function translateKey(phrase, subs)
+    if type(phrase) ~= "string" then
+        error("TypeError: translateKey function expects arg #1 to be a string")
+    end
+
+    if not subs then return phrase end
+    local result = phrase
+
+    for k, v in pairs(subs) do
+        local templateToFind = "%%{" .. k .. "}"
+        result = result:gsub(templateToFind, tostring(v))
+    end
+
+    return result
+end
+
+function Elite.Locale.new(_, opts)
+    local self = setmetatable({}, Elite.Locale)
+
+    self.fallback = opts.fallbackLang and Elite.Locale:new({
+        warnOnMissing = false,
+        phrases = opts.fallbackLang.phrases,
+    }) or false
+
+    self.warnOnMissing = type(opts.warnOnMissing) ~= "boolean" and true or opts.warnOnMissing
+
+    self.phrases = {}
+    self:extend(opts.phrases or {})
+
+    return self
+end
+
+function Elite.Locale:extend(phrases, prefix)
+    for key, phrase in pairs(phrases) do
+        local prefixKey = prefix and ("%s.%s"):format(prefix, key) or key
+        if type(phrase) == "table" then
+            self:extend(phrase, prefixKey)
+        else
+            self.phrases[prefixKey] = phrase
+        end
+    end
+end
+
+function Elite.Locale:clear()
+    self.phrases = {}
+end
+
+function Elite.Locale:replace(phrases)
+    phrases = phrases or {}
+    self:clear()
+    self:extend(phrases)
+end
+
+function Elite.Locale:locale(newLocale)
+    if (newLocale) then
+        self.currentLocale = newLocale
+    end
+    return self.currentLocale
+end
+
+function Elite.Locale:t(key, subs)
+    local phrase, result
+    subs = subs or {}
+
+    if type(self.phrases[key]) == "string" then
+        phrase = self.phrases[key]
+    else
+        if self.warnOnMissing then
+            print(("^3Warning: Missing phrase for key: '%s'"):format(key))
+        end
+        if self.fallback then
+            return self.fallback:t(key, subs)
+        end
+        result = key
+    end
+
+    if type(phrase) == "string" then
+        result = translateKey(phrase, subs)
+    end
+
+    return result
+end
+
+function Elite.Locale:has(key)
+    return self.phrases[key] ~= nil
+end
+
+function Elite.Locale:delete(phraseTarget, prefix)
+    if type(phraseTarget) == "string" then
+        self.phrases[phraseTarget] = nil
+    else
+        for key, phrase in pairs(phraseTarget) do
+            local prefixKey = prefix and prefix .. "." .. key or key
+
+            if type(phrase) == "table" then
+                self:delete(phrase, prefixKey)
+            else
+                self.phrases[prefixKey] = nil
+            end
+        end
+    end
+end
