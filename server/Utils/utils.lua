a4b4706231368ee97e747c628280f233d85e9cf0
LifeGoal <35453587+LifeGoal@users.noreply.github.com>
2024-08-26 11:09:36 +0200

    Added more server-sided util functions

--- server/Utils/utils.lua
+++ server/Utils/utils.lua
@@ -1,4 +1,9 @@
 local module = {}
+local nCharset = {}
+local sCharset = {}
+for i = 48, 57 do nCharset[#nCharset + 1] = string.char(i) end
+for i = 65, 90 do sCharset[#sCharset + 1] = string.char(i) end
+for i = 97, 122 do sCharset[#sCharset + 1] = string.char(i) end
 
 local function tPrint(table, indent)
     indent = indent or 0
@@ -31,6 +36,46 @@ module.debugger = function(table, indent)
     print(("\x1b[4m\x1b[36m[ELITE-LIB DEBUGGER]\x1b[0m"))
     tPrint(table, indent)
     print("\x1b[4m\x1b[36m[DEBUGGER ENDED]\x1b[0m")
+end
+
+module.randomInt = function(length)
+    if not CheckArgs(length) then return end
+    local number = ""
+    for _ = 1, length do
+        number = number..nCharset[math.random(1, #nCharset)]
+    end
+
+    return tonumber(number)
+end
+
+module.randomString = function(length)
+    if not CheckArgs(length) then return end
+    local string = ""
+    for _ = 1, length do
+        string = string..sCharset[math.random(1, #nCharset)]
+    end
+
+    return tostring(string)
+end
+
+module.trim = function(value)
+    if not CheckArgs(value) then return end
+    return (string.gsub(value, '^%s*(.-)%s*$', '%1'))
+end
+
+module.firstToUpper = function(value)
+    if not CheckArgs(value) then return end
+    return (value:gsub("^%l", string.upper))
+end
+
+module.round = function(value, numDecimalPlaces)
+    if not CheckArgs(value) then return end
+    if not numDecimalPlaces then
+        return math.floor(value + 0.5)
+    end
+
+    local power = 10 ^ numDecimalPlaces
+    return (math.floor((value * power) + 0.5) / (power))
 end
 
 return module
