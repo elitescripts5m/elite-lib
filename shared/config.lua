e1033b29840f5a86e7a4acbd78cd53b1946c2a1d
LifeGoal <35453587+LifeGoal@users.noreply.github.com>
2024-08-27 12:05:30 +0200

    Some changes to config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,101 +1,102 @@
-Config = {
-    Settings = {
-        Debug = true,
-        Framework = "auto", --[[
-            Options:
-                "auto",
-                "esx",
-                "qb",
-                "qbox",
-                "custom"
-        ]]
-        Inventory = "qb-inventory", --[[
-            Options:
-                "auto",
-                "esx" (native, not recommended),
-                "qb" (native, not recommended),
-                "ox_inventory",
-                "qb-inventory",
-                "custom"
-        ]]
-        Target = "auto", --[[
-            Options:
-                "auto",
-                "ox",
-                "qb",
-                "custom"
-        ]]
-        Database = "auto", --[[
-            Options:
-                "auto",
-                "esx",
-                "qb",
-                "qbox",
-                "custom"
-        ]]
-        Utils = "utils" -- Don"t change this!
+Config = {}
+
+Config.Settings = {
+    Debug = true,
+    Framework = "auto", --[[
+        Options:
+            "auto",
+            "esx",
+            "qb",
+            "qbox",
+            "custom"
+    ]]
+    Inventory = "auto", --[[
+        Options:
+            "auto",
+            "ox_inventory", (Recommended)
+            "qb-inventory",
+            "esx", (native, not recommended)
+            "qb", (native, not recommended)
+            "custom"
+    ]]
+    Target = "auto", --[[
+        Options:
+            "auto",
+            "ox",
+            "qb",
+            "custom"
+    ]]
+    Database = "auto", --[[
+        Options:
+            "auto",
+            "esx",
+            "qb",
+            "qbox",
+            "custom"
+    ]]
+    Utils = "utils" -- Don"t change this!
+}
+
+Config.Modules = {
+    Framework = {
+        client = {
+            ["es_extended"] = "esx.lua",
+            ["qb-core"] = "qb.lua",
+            ["qbx_core"] = "qbx.lua",
+            ["custom"] = "custom.lua"
+        },
+        server = {
+            ["es_extended"] = "esx.lua",
+            ["qb-core"] = "qb.lua",
+            ["qbx_core"] = "qbx.lua",
+            ["custom"] = "custom.lua"
+        },
     },
-    Modules = {
-        Framework = {
-            client = {
-                ["es_extended"] = "esx.lua",
-                ["qb-core"] = "qb.lua",
-                ["qbx_core"] = "qbx.lua",
-                ["custom"] = "custom.lua"
-            },
-            server = {
-                ["es_extended"] = "esx.lua",
-                ["qb-core"] = "qb.lua",
-                ["qbx_core"] = "qbx.lua",
-                ["custom"] = "custom.lua"
-            },
+    Inventory = {
+        client = {
+            ["ox_inventory"] = "ox_inventory.lua",
+            ["qb-inventory"] = "qb-inventory.lua",
+            ["qb-core"] = "qb.lua",
+            ["es_extended"] = "esx.lua",
+            ["custom"] = "custom.lua"
         },
-        Inventory = {
-            client = {
-                ["ox_inventory"] = "ox_inventory.lua",
-                ["qb-inventory"] = "qb-inventory.lua",
-                ["qb-core"] = "qb.lua",
-                ["es_extended"] = "esx.lua",
-                ["custom"] = "custom.lua"
-            },
-            server = {
-                ["ox_inventory"] = "ox_inventory.lua",
-                ["qb-inventory"] = "qb-inventory.lua",
-                ["qb-core"] = "qb.lua",
-                ["es_extended"] = "esx.lua",
-                ["custom"] = "custom.lua"
-            },
+        server = {
+            ["ox_inventory"] = "ox_inventory.lua",
+            ["qb-inventory"] = "qb-inventory.lua",
+            ["qb-core"] = "qb.lua",
+            ["es_extended"] = "esx.lua",
+            ["custom"] = "custom.lua"
         },
-        Target = {
-            client = {
-                ["ox_target"] = "ox.lua",
-                ["qb-target"] = "qb.lua",
-                ["custom"] = "custom.lua"
-            }
+    },
+    Target = {
+        client = {
+            ["ox_target"] = "ox.lua",
+            ["qb-target"] = "qb.lua",
+            ["custom"] = "custom.lua"
+        }
+    },
+    Database = {
+        server = {
+            ["es_extended"] = "esx.lua",
+            ["qb-core"] = "qb.lua",
+            ["qbx_core"] = "qbx.lua",
+            ["custom"] = "custom.lua"
+        }
+    },
+    Progressbar = {
+        client = {
+            ["progressbar"] = "qb.lua",
+            ["es_extended"] = "esx.lua",
+            ["ox_lib"] = "ox.lua",
+            ["custom"] = "custom.lua"
+        }
+    },
+    Utils = {
+        client = {
+            ["utils"] = "utils.lua"
         },
-        Database = {
-            server = {
-                ["es_extended"] = "esx.lua",
-                ["qb-core"] = "qb.lua",
-                ["qbx_core"] = "qbx.lua",
-                ["custom"] = "custom.lua"
-            }
-        },
-        Progressbar = {
-            client = {
-                ["progressbar"] = "qb.lua",
-                ["es_extended"] = "esx.lua",
-                ["ox_lib"] = "ox.lua",
-                ["custom"] = "custom.lua"
-            }
-        },
-        Utils = {
-            client = {
-                ["utils"] = "utils.lua"
-            },
-            server = {
-                ["utils"] = "utils.lua"
-            }
+        server = {
+            ["utils"] = "utils.lua"
         }
     }
 }
